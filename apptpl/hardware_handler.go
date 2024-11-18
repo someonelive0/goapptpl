db53a863c61f0860276bc856a1e26be224f6d8aa
Lizhi <lizhi2754@idss-cn.com>
2024-11-18 18:53:55 +0800

    add hardware handler

---
+++ apptpl/hardware_handler.go
@@ -0,0 +1,250 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/jaypipes/ghw"
+	log "github.com/sirupsen/logrus"
+)
+
+type HardwareHandler struct {
+}
+
+// r := app.Group("/hardware")
+func (p *HardwareHandler) AddRouter(r fiber.Router) error {
+	log.Info("MysqlHandler AddRouter")
+
+	r.Get("/cpu", p.cpuHandler)
+	r.Get("/mem", p.memHandler)
+	r.Get("/block", p.blockHandler)
+	r.Get("/network", p.networkHandler)
+	r.Get("/pci", p.pciHandler)
+	r.Get("/gpu", p.gpuHandler)
+	r.Get("/chassis", p.chassisHandler)
+	r.Get("/bios", p.biosHandler)
+	r.Get("/baseboard", p.baseboardHandler)
+	r.Get("/product", p.productHandler)
+
+	return nil
+}
+
+// GET /hardware/cpu
+func (p *HardwareHandler) cpuHandler(c fiber.Ctx) error {
+	cpu, err := ghw.CPU()
+	if err != nil {
+		log.Errorf("Error getting CPU info: %v", err)
+		return err
+	}
+	// c.WriteString(fmt.Sprintf("cpu %#v\n", cpu))
+
+	c.Context().SetContentType("application/json")
+	b, _ := json.Marshal(cpu)
+	c.Write(b)
+
+	// for _, proc := range cpu.Processors {
+	// 	c.WriteString(fmt.Sprintf("proc %v\n", proc))
+	// 	for _, core := range proc.Cores {
+	// 		c.WriteString(fmt.Sprintf("core  %v\n", core))
+	// 	}
+	// 	if len(proc.Capabilities) > 0 {
+	// 		// pretty-print the (large) block of capability strings into rows
+	// 		// of 6 capability strings
+	// 		rows := int(math.Ceil(float64(len(proc.Capabilities)) / float64(6)))
+	// 		for row := 1; row < rows; row = row + 1 {
+	// 			rowStart := (row * 6) - 1
+	// 			rowEnd := int(math.Min(float64(rowStart+6), float64(len(proc.Capabilities))))
+	// 			rowElems := proc.Capabilities[rowStart:rowEnd]
+	// 			capStr := strings.Join(rowElems, " ")
+	// 			if row == 1 {
+	// 				fmt.Printf("  capabilities: [%s\n", capStr)
+	// 			} else if rowEnd < len(proc.Capabilities) {
+	// 				fmt.Printf("                 %s\n", capStr)
+	// 			} else {
+	// 				fmt.Printf("                 %s]\n", capStr)
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	return nil
+}
+
+// GET /hardware/mem
+func (p *HardwareHandler) memHandler(c fiber.Ctx) error {
+	memory, err := ghw.Memory()
+	if err != nil {
+		log.Errorf("Error getting memory info: %v", err)
+		return err
+	}
+
+	c.WriteString(fmt.Sprintf(`{ "memo": "%s", `, memory.String()))
+
+	c.Context().SetContentType("application/json")
+	b, _ := json.Marshal(memory)
+	c.Write(b[1:])
+
+	return nil
+}
+
+// GET /hardware/block
+func (p *HardwareHandler) blockHandler(c fiber.Ctx) error {
+	block, err := ghw.Block()
+	if err != nil {
+		log.Errorf("Error getting block storage info: %v", err)
+		return err
+	}
+
+	c.Context().SetContentType("application/json")
+	b, _ := json.Marshal(block)
+	c.Write(b)
+
+	// for _, disk := range block.Disks {
+	// 	fmt.Printf("2 %v\n", disk)
+	// 	for _, part := range disk.Partitions {
+	// 		fmt.Printf("3  %v\n", part)
+	// 	}
+	// }
+
+	return nil
+}
+
+// GET /hardware/network
+func (p *HardwareHandler) networkHandler(c fiber.Ctx) error {
+	net, err := ghw.Network()
+	if err != nil {
+		log.Errorf("Error getting network info: %v", err)
+		return err
+	}
+
+	c.Context().SetContentType("application/json")
+	b, _ := json.Marshal(net)
+	c.Write(b)
+
+	// for _, nic := range net.NICs {
+	// 	fmt.Printf("2  %v\n", nic)
+
+	// 	enabledCaps := make([]int, 0)
+	// 	for x, cap := range nic.Capabilities {
+	// 		if cap.IsEnabled {
+	// 			enabledCaps = append(enabledCaps, x)
+	// 		}
+	// 	}
+	// 	if len(enabledCaps) > 0 {
+	// 		fmt.Printf("  enabled capabilities:\n")
+	// 		for _, x := range enabledCaps {
+	// 			fmt.Printf("   - %s\n", nic.Capabilities[x].Name)
+	// 		}
+	// 	}
+	// }
+
+	return nil
+}
+
+// GET /hardware/pci
+func (p *HardwareHandler) pciHandler(c fiber.Ctx) error {
+	pci, err := ghw.PCI()
+	if err != nil {
+		log.Errorf("Error getting PCI info: %v", err)
+		return err
+	}
+	fmt.Printf("host PCI devices:\n")
+	fmt.Println("====================================================")
+
+	c.Context().SetContentType("application/json")
+	b, _ := json.Marshal(pci)
+	c.Write(b)
+
+	// for _, device := range pci.Devices {
+	// 	vendor := device.Vendor
+	// 	vendorName := vendor.Name
+	// 	if len(vendor.Name) > 20 {
+	// 		vendorName = string([]byte(vendorName)[0:17]) + "..."
+	// 	}
+	// 	product := device.Product
+	// 	productName := product.Name
+	// 	if len(product.Name) > 40 {
+	// 		productName = string([]byte(productName)[0:37]) + "..."
+	// 	}
+	// 	fmt.Printf("%-12s\t%-20s\t%-40s\n", device.Address, vendorName, productName)
+	// }
+
+	return nil
+}
+
+// GET /hardware/gpu
+func (p *HardwareHandler) gpuHandler(c fiber.Ctx) error {
+	gpu, err := ghw.GPU()
+	if err != nil {
+		log.Errorf("Error getting GPU info: %v", err)
+		return err
+	}
+
+	c.Context().SetContentType("application/json")
+	b, _ := json.Marshal(gpu)
+	c.Write(b)
+
+	// for _, card := range gpu.GraphicsCards {
+	// 	fmt.Printf(" %v\n", card)
+	// }
+
+	return nil
+}
+
+// GET /hardware/chassis
+func (p *HardwareHandler) chassisHandler(c fiber.Ctx) error {
+	chassis, err := ghw.Chassis()
+	if err != nil {
+		log.Errorf("Error getting chassis info: %v", err)
+		return err
+	}
+
+	c.Context().SetContentType("application/json")
+	b, _ := json.Marshal(chassis)
+	c.Write(b)
+
+	return nil
+}
+
+// GET /hardware/bios
+func (p *HardwareHandler) biosHandler(c fiber.Ctx) error {
+	bios, err := ghw.BIOS()
+	if err != nil {
+		fmt.Printf("Error getting BIOS info: %v", err)
+	}
+
+	c.Context().SetContentType("application/json")
+	b, _ := json.Marshal(bios)
+	c.Write(b)
+
+	return nil
+}
+
+// GET /hardware/baseboard
+func (p *HardwareHandler) baseboardHandler(c fiber.Ctx) error {
+	baseboard, err := ghw.Baseboard()
+	if err != nil {
+		fmt.Printf("Error getting baseboard info: %v", err)
+	}
+
+	c.Context().SetContentType("application/json")
+	b, _ := json.Marshal(baseboard)
+	c.Write(b)
+
+	return nil
+}
+
+// GET /hardware/product
+func (p *HardwareHandler) productHandler(c fiber.Ctx) error {
+	product, err := ghw.Product()
+	if err != nil {
+		fmt.Printf("Error getting product info: %v", err)
+	}
+
+	c.Context().SetContentType("application/json")
+	b, _ := json.Marshal(product)
+	c.Write(b)
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/gofiber/fiber/v3 v3.0.0-beta.3
 	github.com/gomutex/godocx v0.1.5
+	github.com/jaypipes/ghw v0.13.0
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	github.com/lib/pq v1.10.9
 	github.com/minio/minio-go/v7 v7.0.80
@@ -24,6 +25,7 @@ require (
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/ClickHouse/ch-go v0.61.5 // indirect
+	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/andybalholm/brotli v1.1.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
@@ -31,9 +33,11 @@ require (
 	github.com/go-faster/city v1.0.1 // indirect
 	github.com/go-faster/errors v0.7.1 // indirect
 	github.com/go-ini/ini v1.67.0 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/gofiber/utils/v2 v2.0.0-beta.7 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/jaypipes/pcidb v1.0.1 // indirect
 	github.com/jonboulle/clockwork v0.4.0 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
@@ -41,6 +45,7 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/paulmach/orb v0.11.1 // indirect
 	github.com/pierrec/lz4/v4 v4.1.21 // indirect
@@ -62,4 +67,5 @@ require (
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	howett.net/plist v1.0.0 // indirect
 )
